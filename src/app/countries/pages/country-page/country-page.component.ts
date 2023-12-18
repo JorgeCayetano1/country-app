d1450774d6c3a05f017f0cf51fad2ab49dd312b3
Jorge Enrique Cayetano Arevalo (Platino247) <dt.programadorPL2@upch.pe>
2023-12-18 17:44:02 -0500

    continues learning

--- src/app/countries/pages/country-page/country-page.component.ts
+++ src/app/countries/pages/country-page/country-page.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { OnInit, Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { CountriesService } from '../../services/countries.service';
 
 @Component({
   selector: 'app-country-page',
   templateUrl: './country-page.component.html',
-  styles: ``
+  styles: ``,
 })
-export class CountryPageComponent {
+export class CountryPageComponent implements OnInit {
+  constructor(
+    private activateRoute: ActivatedRoute,
+    private countriesService: CountriesService
+  ) {}
 
+  ngOnInit(): void {
+    this.activateRoute.params.subscribe(({ id }) => {
+      console.log({ params: id });
+    });
+  }
 }
--- src/app/countries/services/countries.service.ts
+++ src/app/countries/services/countries.service.ts
@@ -9,8 +9,39 @@ export class CountriesService {
 
   constructor(private http: HttpClient) {}
 
+  searchCountryAlphaCode(code: string): Observable<Country[]> {
+    const url = `${this.apiUrl}/alpha/${code}`;
+
+    return this.http.get<Country[]>(url).pipe(catchError(() => of([])));
+  }
+
+  // capital
   searchCapital(term: string): Observable<Country[]> {
     const url = `${this.apiUrl}/capital/${term}`;
+
+    return this.http.get<Country[]>(url).pipe(
+      catchError((err) => {
+        console.log('Error:', err);
+        return of([]);
+      })
+    );
+  }
+
+  // country
+  searchCountry(term: string): Observable<Country[]> {
+    const url = `${this.apiUrl}/name/${term}`;
+
+    return this.http.get<Country[]>(url).pipe(
+      catchError((err) => {
+        console.log('Error:', err);
+        return of([]);
+      })
+    );
+  }
+
+  // region
+  searchRegion(term: string): Observable<Country[]> {
+    const url = `${this.apiUrl}/region/${term}`;
 
     return this.http.get<Country[]>(url).pipe(
       catchError((err) => {
